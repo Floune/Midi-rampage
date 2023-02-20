c9f4274071ccb93f17d06e8d7576c42ae86c9175
Allan <allan.cerveaux@solotech.com>
2023-02-20 10:24:20 +0100

    Add vite pour être rapide et furieux

---
+++ vite.config.js
@@ -0,0 +1,29 @@
+import { defineConfig } from 'vite';
+import eslint from 'vite-plugin-eslint';
+import { resolve } from 'path';
+
+export default defineConfig({
+  plugins: [eslint()],
+  resolve: {
+    alias: {
+      '@assets': resolve(__dirname, './src/assets'),
+      '@game': resolve(__dirname, './src/scripts'),
+      '@scenes': resolve(__dirname, './src/scripts/scenes'),
+      '@objects': resolve(__dirname, './src/scripts/objects'),
+      '@utils': resolve(__dirname, './src/scripts/utils'),
+      '@decorators': resolve(__dirname, './src/scripts/decorators'),
+      '@constants': resolve(__dirname, './src/scripts/constants.ts'),
+      '@types': resolve(__dirname, './src/scripts/types.ts')
+    }
+  },
+  build: {
+    // phaser doesn't accept inlined assets
+    assetsInlineLimit: 0
+  },
+  server: {
+    host: process.env.VITE_HOST ? process.env.VITE_HOST : '0.0.0.0',
+    port: 3000,
+    open: '/'
+  },
+  clearScreen: false
+});
