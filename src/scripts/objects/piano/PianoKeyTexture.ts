f08cc6afe5bc303c64d8677b0f1343291a580082
Allan <allan.cerveaux@solotech.com>
2023-02-21 11:35:07 +0100

    set width and height of texture it's better now

--- src/scripts/objects/piano/PianoKeyTexture.ts
+++ src/scripts/objects/piano/PianoKeyTexture.ts
@@ -12,14 +12,14 @@ export default class PianoKeyTexture extends Phaser.GameObjects.Graphics {
       const sharpTexture = key.endsWith('sharp');
       const tileWidth = sharpTexture ? 30 : 50;
       const tileHeight = sharpTexture ? 150 : 200;
-      const tileColor = sharpTexture ? 0x000000 : 0xffffff;
+      const tileColor = sharpTexture ? 0x383838 : 0xffffff;
 
       this.fillStyle(tileColor, 1.0);
       this.fillRect(x || 0, y || 0, tileWidth, tileHeight);
       this.lineStyle(1, 0x555555, 1);
       this.strokeRect(x || 0, y || 0, tileWidth, tileHeight);
 
-      this.generateTexture(key);
+      this.generateTexture(key, tileWidth, tileHeight);
     });
   }
 }
