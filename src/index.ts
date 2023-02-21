02913b1e5ad83e95e5fcaa0bac3298ad7fd4912d
Allan <allan.cerveaux@solotech.com>
2023-02-21 14:40:09 +0100

    add global interface

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,26 @@
 import 'phaser';
+import type { IMIDIInput } from '@midival/core';
 import { MIDIVal } from '@midival/core';
 
 import config from '@game/config';
 
-window.allDevices = [];
-navigator.permissions.query({ name: 'midi', sysex: true }).then((result) => {
+// @TODO: need to move in custom.d.ts not work IDKW
+declare global {
+  interface Window {
+    devices: IMIDIInput[];
+  }
+}
+
+window.devices = [];
+const permissionName = 'midi' as PermissionName;
+navigator.permissions.query({ name: permissionName }).then((result) => {
   if (result.state === 'granted') {
     MIDIVal.connect()
       .then((accessObject) => {
         console.log('Inputs', accessObject.inputs);
         console.log('Outputs', accessObject.outputs);
+        window.devices = accessObject.inputs;
 
-        window.allDevices = accessObject.inputs;
         new Phaser.Game(config);
       })
       .catch((err) => {
