8b23e4d52089edc290a1868bc43a270e623c6fe3
Allan <allan.cerveaux@solotech.com>
2023-02-21 23:15:11 +0100

    remove type and add in custom.d.ts and set MIDIVal in ScenePlugin

--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,12 @@
 import 'phaser';
-import type { IMIDIInput } from '@midival/core';
-import { MIDIVal } from '@midival/core';
 
 import config from '@game/config';
-
-// @TODO: need to move in custom.d.ts not work IDKW
-declare global {
-  interface Window {
-    devices: IMIDIInput[];
-  }
-}
 
 window.devices = [];
 const permissionName = 'midi' as PermissionName;
 navigator.permissions.query({ name: permissionName }).then((result) => {
   if (result.state === 'granted') {
-    MIDIVal.connect()
-      .then((accessObject) => {
-        console.log('Inputs', accessObject.inputs);
-        console.log('Outputs', accessObject.outputs);
-        window.devices = accessObject.inputs;
-
-        new Phaser.Game(config);
-      })
-      .catch((err) => {
-        console.log('Error', err);
-      });
+    new Phaser.Game(config);
   } else if (result.state === 'prompt') {
     console.log('bonjour');
   }
