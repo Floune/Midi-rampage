bbb5491fabe473e2ab6488368321ba5a94bdec3b
Allan <allan.cerveaux@solotech.com>
2023-02-21 10:04:35 +0100

    Make texture piano texture key

--- src/scripts/objects/piano/PianoKeyTexture.ts
+++ src/scripts/objects/piano/PianoKeyTexture.ts
@@ -3,23 +3,23 @@ type PianoTextureType = {
   x?: number;
   y?: number;
   options?: Phaser.Types.GameObjects.Graphics.Options | undefined;
-  sharp: boolean;
 };
 
 export default class PianoKeyTexture extends Phaser.GameObjects.Graphics {
-  constructor({ scene, x, y, options, sharp }: PianoTextureType) {
+  constructor({ scene, x, y, options }: PianoTextureType) {
     super(scene, { ...options, x, y });
+    ['piano_key_sharp', 'piano_key'].forEach((key) => {
+      const sharpTexture = key.endsWith('sharp');
+      const tileWidth = sharpTexture ? 30 : 50;
+      const tileHeight = sharpTexture ? 150 : 200;
+      const tileColor = sharpTexture ? 0x000000 : 0xffffff;
 
-    const key = sharp ? 'piano_key_sharp' : 'piano_key';
-    const tileWidth = sharp ? 30 : 50;
-    const tileHeight = sharp ? 150 : 200;
-    const tileColor = sharp ? 0x000000 : 0xffffff;
+      this.fillStyle(tileColor, 1.0);
+      this.fillRect(x || 0, y || 0, tileWidth, tileHeight);
+      this.lineStyle(1, 0x555555, 1);
+      this.strokeRect(x || 0, y || 0, tileWidth, tileHeight);
 
-    this.fillStyle(tileColor, 1.0);
-    this.fillRect(x || 0, y || 0, tileWidth, tileHeight);
-    this.lineStyle(1, 0x555555, 1);
-    this.strokeRect(x || 0, y || 0, tileWidth, tileHeight);
-
-    this.generateTexture(key);
+      this.generateTexture(key);
+    });
   }
 }
