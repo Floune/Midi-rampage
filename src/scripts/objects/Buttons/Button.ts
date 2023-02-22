44a2c2050d927918d74f5b1e9da734c16ede4567
Allan <allan.cerveaux@solotech.com>
2023-02-22 11:40:18 +0100

    make a Button to select device TODO: improve UI

---
+++ src/scripts/objects/Buttons/Button.ts
@@ -0,0 +1,52 @@
+type ButtonType = {
+  scene: Phaser.Scene;
+  x: number;
+  y: number;
+  text?: string;
+  data?: any;
+};
+
+export class Button extends Phaser.GameObjects.GameObject {
+  background: Phaser.GameObjects.Graphics;
+  text: Phaser.GameObjects.Text;
+  constructor({ scene, x, y, text, data }: ButtonType) {
+    super(scene, 'button');
+    this.scene.add.existing(this);
+
+    this.background = this.scene.add.graphics();
+    this.background.fillStyle(0xe53935);
+
+    this.text = this.scene.add
+      .text(x, y, text || 'button', {
+        color: '#FFF',
+        fontSize: '24px',
+        backgroundColor: '#e53935',
+        padding: {
+          top: 5,
+          bottom: 5,
+          left: 5,
+          right: 5
+        },
+        wordWrap: {
+          width: 150
+        },
+        align: 'center'
+      })
+      .setOrigin(0.5);
+
+    this.text.setInteractive();
+
+    this.text.on('pointerover', () => {
+      this.text.setBackgroundColor('#c62828');
+    });
+
+    this.text.on('pointerout', () => {
+      this.text.setBackgroundColor('#e53935');
+    });
+
+    this.text.on('pointerdown', () => {
+      this.text.setBackgroundColor('#e57373');
+      this.scene.sys.events.emit('onButtonClick', { data });
+    });
+  }
+}
